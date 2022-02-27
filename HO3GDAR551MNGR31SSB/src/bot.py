1aa243f1345bbb1fa01995a2b2b258da983d2c03
MagmaMustang123 <80600048+MagmaMustang123@users.noreply.github.com>
2022-02-27 18:01:33 +0300

    Add files via upload

---
+++ HO3GDAR551MNGR31SSB/src/bot.py
@@ -0,0 +1,38 @@
+from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
+from rlbot.messages.flat.QuickChatSelection import QuickChatSelection
+from rlbot.utils.structures.game_data_struct import GameTickPacket
+from util.ball_prediction_analysis import find_slice_at_time
+from util.boost_pad_tracker import BoostPadTracker
+from util.drive import steer_toward_target
+from util.sequence import Sequence, ControlStep
+from util.vec import Vec3 
+from utils.dodge import *
+from objects import agent
+from utils.kickoff import *
+from utils.dodge import *
+
+
+
+packet=GameTickPacket()
+
+class MyBot(BaseAgent):
+    def __init__(self, name, team, index):
+        super().__init__(name, team, index)
+        self.active_sequence: Sequence = None
+        self.boost_pad_tracker = BoostPadTracker()
+        global my_car, car_location, car_velocity, ball_location
+        my_car = packet.game_cars[self.index]
+        car_location = Vec3(my_car.physics.location)
+        car_velocity = Vec3(my_car.physics.velocity)
+        ball_location = Vec3(packet.game_ball.physics.location)
+        
+    def initialize_agent(self):
+        # Set up information about the boost pads now that the game is active and the info is available
+        self.boost_pad_tracker.initialize_boosts(self.get_field_info())
+
+    def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
+        if self.kickoff_flag:
+            self.push(kickoff)
+        #else:
+            #self.push(dodge)
+        return controls
